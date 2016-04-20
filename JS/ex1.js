392d69f6b879554a9e2b836bd1cd24abfc7e5e28
a1420267 <a1420267@192.168.160.17>
2016-04-20 20:33:57 -0300

    Comitt final da Aula de 20/04/2016

---
+++ JS/ex1.js
@@ -0,0 +1,32 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow > 18) {
+    greeting = 'Good evening!';
+} else if (hourNow > 12) {
+    greeting = 'Good afternoon!';
+} else if (hourNow > 6) {
+    greeting = 'Good morning!';
+} else {
+    greeting = 'Welcome!';
+}
+
+document.write
+('<h3>' + greeting + '</h3>');
+
+function sayHello(teste){
+    window.alert("Testando" + teste);
+    
+}
+
+function helloInHTML(name){
+    document.getElementById("result");
+}
--- index.php
+++ index.php
@@ -7,11 +7,16 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
+        <link href="CSS/ex1.css" rel="stylesheet">
+        <script type="text/javascript" src="JS/ex1.js"> </script>
         <title></title>
     </head>
     <body>
+        <p>dwe</p>
+        
+        <button onclick="sayHello('world')">Clica aqui</button>
         <?php
-        // put your code here
+        echo "DWE";
         ?>
     </body>
 </html>
